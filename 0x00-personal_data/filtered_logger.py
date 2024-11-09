e0246bea715deff579d5e6a5dc5801f4172ff25a
Patrick Odeh <vyltyan@gmail.com>
2024-11-09 14:29:46 +0100

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,9 +2,10 @@
 """Regex: Returning log messages"""
 
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """Obfuscates specified fields in a log message."""
     return re.sub(rf"({'|'.join(fields)})=[^{separator}]+",
                   lambda m: f"{m.group(1)}={redaction}", message)
